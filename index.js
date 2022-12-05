5b8f4d85a32a0a390044a068e5d3f9445553d541
topher ludlow <topherludlow@protonmail.com>
2022-12-05 01:18:23 -0800

    commit tree 2c276f13154bbd94afd0a6b7db1690474e53a1a2

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Code your solutions in this file
+const names = ["Guadalupe", "Ollie", "Aki"];
+const event = 'surprise';
+
+writeCards(array);
+countDown(number);
+
+function writeCards(names, event){
+    const array = [];
+    for (let i = 0; i < names.length; i++){
+        array.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+    }
+    return array;
+}
+
+function countDown(number){
+    while (number >= 0){
+        console.log(number);
+        number-= 1;
+    }
+}
